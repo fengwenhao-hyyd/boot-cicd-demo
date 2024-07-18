48742cfd00eab73f1aea523b9615cebdb87e1cd4
fengwenhao-hyyd <124327879+fengwenhao-hyyd@users.noreply.github.com>
2024-07-18 11:17:36 +0900

    Update PingController.java
    update by others

--- src/main/java/icu/idontwant996/bootcicddemo/controller/PingController.java
+++ src/main/java/icu/idontwant996/bootcicddemo/controller/PingController.java
@@ -9,6 +9,7 @@ public class PingController {
 
     @GetMapping("/ping/{param}")
     public String ping(@PathVariable String param) {
+        System.out.println("some new feature from other member");
         return "Response is: (" + param + ")";
     }
 
